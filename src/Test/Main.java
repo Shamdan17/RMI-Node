73d43394aa0195d1bfbd14829a6948f0f6e7bfd9
shamdan17 <shamdan17@ku.edu.tr>
2019-04-09 21:39:04 +0300

    Added more functionality to main method for testing

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -8,19 +8,19 @@ import RMI.RMIServer;
 public class Main {
 	
 	public static void main(String args[]) {
-		String childIP = "172.20.120.155";//Set to ROOT if no child (I know this is wrong but whatever)
-		int sendmessage = 0;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the ip address you want to connect to (leaf if none): ");
+		String childIP = in.nextLine();//Set to leaf if no child
+		System.out.println("Enter any number other than -1 to be able to send a message");
+		int sendmessage = in.nextInt();
 		try {
 			RMIServer server = new RMIServer();
-			if(!childIP.equalsIgnoreCase("root")) {
+			if(!childIP.equalsIgnoreCase("leaf")) {
 				server.addChild(childIP);
-				//server.addChild("172.20.128.207");
 			}
-
 			Naming.rebind("//localhost/RMIImpl", server);//Bind the server to make it available to connect to
 			System.out.println("Rebinding successful. Server is in operation");
 			
-			Scanner in = new Scanner(System.in);
 			while(sendmessage!=-1) {
 				String message = in.nextLine();
 				if(!message.equalsIgnoreCase("Exit"));
